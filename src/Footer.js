f9405940aff2289ee39d569e9e92d2be0236ac30
emily-ecms <80700679+emily-ecms@users.noreply.github.com>
2022-05-14 00:21:06 +0100

    Changed styling, added Footer component

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from "react"; 
+
+export default function Footer() {
+    return (
+        <p><a href="https://github.com/emily-ecms/weather-react-app" target="_blank"> Open Sourced Code</a> by Emily Sleightholme</p>
+    )
+}
