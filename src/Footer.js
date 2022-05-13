0d3a22cfeace388840979546600a7b8db444cb86
emily-ecms <80700679+emily-ecms@users.noreply.github.com>
2022-05-14 00:30:08 +0100

    Edited footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <p><a href="https://github.com/emily-ecms/weather-react-app" target="_blank"> Open Sourced Code</a> by Emily Sleightholme</p>
+        <p><a href="https://github.com/emily-ecms/weather-react-app" target="_blank" rel="noreferrer"> Open Sourced Code</a> by Emily Sleightholme</p>
     )
 }
