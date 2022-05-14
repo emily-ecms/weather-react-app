ea4b60a30af574ea5f752bedc4f08e57cadbd78b
emily-ecms <80700679+emily-ecms@users.noreply.github.com>
2022-05-14 11:36:41 +0100

    Created basic weather search form

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <h1>My weather App </h1>
+        <h1>Weather Search Engine </h1>
         <Weather />
         <Footer />
       </header>
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+//import axios from "axios";
 
 export default function Weather() {
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState({});
+    const [dataShowing, setDataShowing] = useState(false);
+
+    const form = (<form onSubmit={searchCity}>
+                <input type="search" placeholder="Search city" onChange={updateCity}></input>
+                <button>Search</button>
+            </form>)
+
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+
+    function searchCity(event) {
+        event.preventDefault();
+        setWeather(`Weather is hot in ${city}` );
+        setDataShowing(true);
+    }
+
+    if(dataShowing) {
     return(
-        <h2>Weather here</h2>
+        <div>
+        {form}
+        <h2>{weather}</h2>
+        </div>
     )
+    }
+    else {
+        return(
+            <div>
+            {form}
+            </div>
+        )
+    }
 }
