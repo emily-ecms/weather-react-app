8e468aefa1e873773eb72ed34e358824e80d0d1e
emily-ecms <80700679+emily-ecms@users.noreply.github.com>
2022-05-16 20:12:26 +0100

    Added formatted date & put weather data in separate component

--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./Weather.css"
-
-//import axios from "axios";
+import WeatherData from "./WeatherData";
 
 export default function Weather() {
     const [city, setCity] = useState("");
@@ -19,9 +18,7 @@ export default function Weather() {
             <button type="submit" className="btn btn-primary w-100">Search</button>
             </div>
             </div>
-            
-            </form>
-     
+    </form>
   );
 
     function updateCity(event) {
@@ -30,55 +27,30 @@ export default function Weather() {
 
     function setData(response) {
         setWeather({
-            dataShowing: true,
+            date: new Date(response.data.dt * 1000),
             city: response.data.name,
             description: response.data.weather[0].description,
             temp: response.data.main.temp,
             humidity: response.data.main.humidity,
             wind: response.data.wind.speed,
-            icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-        })
-        
-       
+            icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            dataShowing: true
+        })  
     }
 
     function searchCity(event) {
         event.preventDefault();
-        
-
-        let apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
-        let units ="metric";
+        const apiKey = `5f472b7acba333cd8a035ea85a0d4d4c`;
+        const units ="metric";
         let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
         axios.get(apiUrl).then(setData);
-
-        //setDataShowing(true);
     }
 
     if(weather.dataShowing) {
     return(
         <div className="Weather">
         {form}
-        <h2 className="city mt-4 mb-2"> {weather.city}</h2>
-        <p className="description mb-2">{weather.description}</p>
-        <div className="row">
-
-            <div className="col">
-            <div className="temperature">{Math.round(weather.temp)}<span className="unit"> °C </span></div>
-            </div>
-
-            <div className="col">
-            <img src={weather.icon} alt={weather.description} className="icon"/> 
-            </div>
-
-            <div className="col">
-            <ul className="weatherData">
-                <li>Humidity: {weather.humidity} %</li>
-                <li>Wind: {Math.round(weather.wind)} km/h</li>
-            </ul>
-            </div>
-
-        </div>
+       <WeatherData weather={weather} />
        
         <div className="row forecast">
 
---
+++ src/WeatherData.js
@@ -0,0 +1,35 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherData(props) {
+   
+
+    return (
+        <div className="WeatherData">
+            
+        
+        <h2 className="city mt-4 mb-2"> {props.weather.city}</h2>
+        <FormattedDate date={props.weather.date} />
+        <p className="description mb-2">{props.weather.description}</p>
+        <div className="row">
+
+            <div className="col">
+            <div className="temperature">{Math.round(props.weather.temp)}<span className="unit"> °C </span></div>
+            </div>
+
+            <div className="col">
+            <img src={props.weather.icon} alt={props.weather.description} className="icon"/> 
+            </div>
+
+            <div className="col">
+            <ul className="weatherData">
+                <li>Humidity: {props.weather.humidity} %</li>
+                <li>Wind: {Math.round(props.weather.wind)} km/h</li>
+            </ul>
+            </div>
+
+            </div>
+            </div>
+        
+    )
+}
