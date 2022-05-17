905d4a3486f814a647f678bb4b8fc7e8244617e9
emily-ecms <80700679+emily-ecms@users.noreply.github.com>
2022-05-17 21:05:37 +0100

    Made code cleaner & more concise

--- src/Temperature.js
+++ src/Temperature.js
@@ -2,38 +2,27 @@ import React, { useState } from "react";
 import "./Temperature.css";
 
 export default function Temperature(props) {
+    const [unit, setUnit] = useState("celsius");
     
-
-    const [unit, setUnit] = useState("celcius");
-    //const [clickedUnitLink, setClickedUnitLink] = useState(false);
-   
-    let celciusTemp = props.celcius;
-    let fahrenheitTemp = props.fahrenheit;
-
-    //setTemp(celcius);
-
-
-    //alert(`temperature is ${temperature}`);
-
-     function getFahrenheit() {
+    function getFahrenheit(event) {
+        event.preventDefault();
         setUnit("fahrenheit");
-        
     }
 
-    function getCelcius() {
-       setUnit("celcius");
-        
+    function getCelsius(event) {
+        event.preventDefault();
+       setUnit("celsius");    
     }
 
-    if(unit === "celcius") {
+    if(unit === "celsius") {
     
         return (
-            <div className="Temperature">{celciusTemp}°<span className="unit ms-3"><a className="celcius active" href="#" onClick={getCelcius}>C </a> | <a className="fahrenheit" href="#" onClick={getFahrenheit}>F</a> </span></div> 
+            <div className="Temperature">{props.celsius}°<span className="unit ms-3"><span className="active">C </span> | <a href="#" onClick={getFahrenheit}>F</a> </span></div> 
         )
     
         } else {
             return (
-                <div className="Temperature">{fahrenheitTemp}°<span className="unit ms-3"><a className="celcius" href="#" onClick={getCelcius}>C </a> | <a className="fahrenheit active" href="#" onClick={getFahrenheit}>F</a> </span></div> 
+                <div className="Temperature">{props.fahrenheit}°<span className="unit ms-3"><a href="#" onClick={getCelsius}>C </a> | <span className="active">F</span> </span></div> 
             )
         }
 
--- src/WeatherData.js
+++ src/WeatherData.js
@@ -12,7 +12,7 @@ export default function WeatherData(props) {
         <p className="description mb-2">{props.weather.description}</p>
         <div className="row">
         <div className="col">
-            <Temperature temperature={props.weather.temp} celcius={Math.round(props.weather.temp)} fahrenheit={Math.round(props.weather.temp * 1.8 + 32)}/>
+            <Temperature celsius={Math.round(props.weather.temp)} fahrenheit={Math.round(props.weather.temp * 1.8 + 32)}/>
             
         </div>
         <div className="col">
